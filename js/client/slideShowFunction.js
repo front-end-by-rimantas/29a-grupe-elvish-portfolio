ba1975e6e84e55cfc02909234b18dad0b83c9144
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-07-25 13:22:55 +0300

    Done

--- js/client/slideShowFunction.js
+++ js/client/slideShowFunction.js
@@ -16,6 +16,36 @@ function showSlides() {
 // console.log(slides);
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 6500); // Change image every 2 seconds
+
+
+  dots[0].addEventListener("click", function() {
+      slides[0].style.display = 'block';
+      slides[1].style.display = 'none';
+      slides[2].style.display = 'none';
+      dots[1].className = dots[1].className.replace(" active", "");
+      dots[2].className = dots[2].className.replace(" active", "");
+      dots[0].className += " active";   
+})
+  dots[1].addEventListener("click", function() {
+      slides[1].style.display = 'block';
+      slides[0].style.display = 'none';
+      slides[2].style.display = 'none';
+      dots[0].className = dots[1].className.replace(" active", "");
+      dots[2].className = dots[2].className.replace(" active", "");
+      dots[1].className += " active";   
+        
+})
+  dots[2].addEventListener("click", function() {
+      slides[2].style.display = 'block';
+      slides[0].style.display = 'none';
+      slides[1].style.display = 'none';
+      dots[0].className = dots[1].className.replace(" active", "");
+      dots[1].className = dots[2].className.replace(" active", "");
+      dots[2].className += " active";   
+        
+      
+})
 }
+
 
 export {showSlides}
