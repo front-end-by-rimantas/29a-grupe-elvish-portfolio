55d3b25b61edf9ce94cd515ae43cb7deada93c84
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-07-26 00:39:56 +0300

    Oh yeah

--- js/header/headerTransparentStyle.js
+++ js/header/headerTransparentStyle.js
@@ -5,15 +5,18 @@ function headerStyle() {
         limit = 40,
         element = document.getElementById('main_header'),
         element1 = document.getElementById('logo_light'),
+        element2 = document.getElementById('menu-button-white'),
         clname = '';
     if ( scroll > limit ) {
         clname = ' ' + element.className + ' ';
         clname = clname.replace(" transparent ", " ");
         element.className = clname;
         element1.classList.add("none")
+        element2.classList.add("none")
     } else {
         element.className += ' transparent';
         element1.classList.remove("none")
+        element2.classList.remove("none")
     }
 }
 
